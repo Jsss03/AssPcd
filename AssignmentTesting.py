d7d664c2cda703386a6d8f021948b126e1ecd3e4
Js <goh0109131396@gmail.com>
2026-03-14 19:06:09 +0800

    Update AssignmentTesting.py

--- AssignmentTesting.py
+++ AssignmentTesting.py
@@ -131,6 +131,38 @@ def wave_menu():
     print("")
     print(f"{'='*60}\n")
 
+def get_time_multiplier(timing):
+    time_multiplier = {"morning":0.75,"afternoon":1.0,"evening":0.7,"night":1.15}
+    time_mult=time_multiplier.get(timing)
+    return time_mult
+
+def get_season_multiplier(season):
+    season_multiplier ={"normal":1.0,"rainy":0.85,"festive_holiday":0.80}
+    season_mult=season_multiplier.get(season)
+    return season_mult
+
+def get_traffic_multiplier(traffic):
+    traffic_multiplier = {"light":1.0,"normal":0.9,"Heavy":0.6}
+    traffic_mult=traffic_multiplier.get(traffic)
+    return traffic_mult
+
+def get_base_speed(transport):
+    transport_modes = {"walking":5,"bicycle":15,"car":60,"bus":40}
+    base_speed=transport_modes.get(transport)
+    return base_speed
+    
+def calculate_adjusted_speed():
+    transport=input("Enter transport>>")
+    base_speed=get_base_speed(transport)
+    timing=input("Enter timing>>")
+    time_mult=get_time_multiplier(timing)
+    season=input("Enter season>>")
+    season_mult=get_season_multiplier(season)
+    traffic=input("Enter traffic>>")
+    traffic_mult=get_traffic_multiplier(traffic)
+    adjusted_speed= base_speed * time_mult*season_mult*traffic_mult
+    return adjusted_speed
+    
 def ai_customer_service():
     brain = {"price": "Our travel costs are calculated based on distance. Walking is free!",\
         "weather": "Rain increases travel time by 50% for safety.",\
