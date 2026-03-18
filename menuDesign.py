0247b638dcdff08a402743ad205dca14d8e58638
Js <goh0109131396@gmail.com>
2026-03-19 00:41:16 +0800

    Update menuDesign.py

--- menuDesign.py
+++ menuDesign.py
@@ -24,17 +24,22 @@ def boot_sequence():
         time.sleep(0.1)
     print("\n\n--- ACCESS GRANTED ---\n")
 
-def wave_menu(data):
+def main_menu(data):
     w_lines=wt.get_dashboard_lines(data)
     title = "MAIN MENU"
-    options = ["1.Admin", "2.Estimate", "3.Help Centre", "4.Exit"]
+    options = ["1.Admin Management", "2.Estimate travel time", "3.Help Centre", "4.Exit"]
     print("=" * 70)
     print(f"{title:^70}")
     print("=" * 70)
     for i in range(4):
         # weather_line (30 chars) + Gap + Menu Option
-        weather_part = f"{w_lines[i]}"
-        option_part = f"♦ {options[i]:<35}"
-        print(f"{option_part}  {weather_part}")
-    print("=" * 70)
+        indent=2 if i%2 != 0 else 0
+        base_width=50
+        spacer=" "*indent
+        option_text=f"♦ {options[i]}"
+        weather_text = f"{w_lines[i]}"
+        weather_part=f"{weather_text}"
+        option_part = f"{spacer}{option_text:<{base_width-indent}}"
+        print(f"{option_part}|{weather_part}")
     
+    print("=" * 70)
